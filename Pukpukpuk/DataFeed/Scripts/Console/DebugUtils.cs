3c20efcc9dbc0ea075b59d07a02370347937469c
pukpukpuk <pukpukpuk228@proton.me>
2024-06-25 14:27:49 +0500

    Added docs for DebugUtils.cs

--- Pukpukpuk/DataFeed/Scripts/Console/DebugUtils.cs
+++ Pukpukpuk/DataFeed/Scripts/Console/DebugUtils.cs
@@ -14,12 +14,29 @@ namespace Pukpukpuk.DataFeed.Console
         public const string PlusSign = "<color=#6ec077>+</color>";
         public const string MinusSign = "<color=#cc6666>-</color>";
 
+        /// <summary>
+        /// Log message to DataFeed console
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="layerName">Layer name</param>
+        /// <param name="logMessageType">Type of message</param>
+        /// <param name="prefix">Prefix that will be shown instead of layer name</param>
+        /// <param name="tag">Message tag. Used for hiding specific entries</param>
         public static void LogLayer(object message, string layerName, LogMessageType logMessageType,
             string prefix = null, string tag = null)
         {
             LogLayer(message, layerName, logMessageType.GetHex(), logMessageType, prefix, tag);
         }
 
+        /// <summary>
+        /// Log message to DataFeed console
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="layerName">Layer name</param>
+        /// <param name="textColor">Color of the text</param>
+        /// <param name="logMessageType">Type of message</param>
+        /// <param name="prefix">Prefix that will be shown instead of layer name</param>
+        /// <param name="tag">Message tag. Used for hiding specific entries</param>
         public static void LogLayer(object message, string layerName, string textColor = White,
             LogMessageType logMessageType = LogMessageType.Info, string prefix = null, string tag = null)
         {
@@ -29,11 +46,22 @@ namespace Pukpukpuk.DataFeed.Console
 #endif
         }
 
+        /// <summary>
+        /// Log message to DataFeed console
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="logMessageType">Type of message</param>
         public static void Log(object message, LogMessageType logMessageType)
         {
             Log(message, logMessageType.GetHex(), logMessageType);
         }
 
+        /// <summary>
+        /// Log message to DataFeed console
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="textColor">Color of the text</param>
+        /// <param name="logMessageType">Type of message</param>
         public static void Log(object message, string textColor = White,
             LogMessageType logMessageType = LogMessageType.Info)
         {
